28c2fe3b7444764a9e2a29a10e4ee84644b3b6df
Grzegorz <lpmusicon@gmail.com>
2020-03-05 23:39:18 +0100

    PlatformIO

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello world");
+  delay(1000);
+}
